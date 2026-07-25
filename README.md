46990875522a6963f6a6fbccd897d76e66500768
rigarcia220-byte <rigarcia220@gmail.com>
2026-07-25 16:53:03 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Abra esta pagina en [https://rigarcia220-byte.github.io/primeros-pasos/](https://rigarcia220-byte.github.io/primeros-pasos/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/rigarcia220-byte/primeros-pasos** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/rigarcia220-byte/primeros-pasos** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.py
@@ -0,0 +1,48 @@
+def on_on_overlap(sprite, otherSprite):
+    game.game_over(False)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.food, on_on_overlap)
+
+scene.set_background_color(0)
+jugador1 = sprites.create(img("""
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        """),
+    SpriteKind.player)
+controller.move_sprite(jugador1, 100, 100)
+jugador1.set_position(162, 119)
+jugador1.set_stay_in_screen(True)
+jugador2 = sprites.create(img("""
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        """),
+    SpriteKind.player)
+jugador2.set_position(38, 25)
+jugador2.follow(jugador1, 28)
